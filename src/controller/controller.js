fadc81d1ea2a3ab435571523a98b6fe54453974a
nerdchanii <nerdchanii@gmail.com>
2022-12-19 01:01:30 +0900

    feat(controller):컨트롤러 구현

--- src/controller/controller.js
+++ src/controller/controller.js
@@ -8,8 +8,32 @@ class Controller {
   }
 
   play() {
+    this.view.showServiceStart();
+    this.view.getCoachNames((coachNames) => {
+      coachNames.forEach((coach) => {
+        this.service.addCoach(new Coach(coach));
+      });
+      this.getHateFood();
+    });
   }
 
+  getHateFood(coachIndex = 0) {
+    if (coachIndex === this.service.coaches.length) return  this.generateWeeklyRecommend();
+    const coach = this.service.coaches[coachIndex];
+    this.view.getHateFood(coach.name, (hates) => {
+      coach.addHate(hates);
+      this.getHateFood(coachIndex + 1);
+    });
+  }
+
+  generateWeeklyRecommend() {
+    const [days, categories, coaches] = this.service.getWeeklyRecommend();
+    this.showWeeklyRecommend(days, categories, coaches);
+  }
+
+  showWeeklyRecommend(days, categories, coaches) {
+    this.view.showWeeklyRecommend(days, categories, coaches);
+  }
 }
 
 module.exports = Controller;
